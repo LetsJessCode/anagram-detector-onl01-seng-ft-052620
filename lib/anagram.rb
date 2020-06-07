ee7cd2e4216ad88d3e20f532cc9a308bfccab21d
LetsJessCode <letsjesscode@gmail.com>
2020-06-07 19:16:55 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+         @word = word
+    end 
+
+    def match(detector)
+        detector.select do |element|
+            (@word.split("").sort) == (element.split("").sort)
+        end 
+    end 
+end 
+diaper = Anagram.new("diaper")
+diaper.match(%w(enlists google inlets banana))
